840869015d294e01f474a0bb5df57381267f7276
GET Academy <terje@getacademy.no>
2022-04-26 11:29:09 +0200

    x

---
+++ Measurement/Model/MeasurementStats.cs
@@ -0,0 +1,9 @@
+﻿namespace Measurement.Model
+{
+    public class MeasurementStats
+    {
+        public int Year { get; set; }
+        public float MaxValue { get; set; }
+        public int ValueCount { get; set; }
+    }
+}
--- Measurement/Program.cs
+++ Measurement/Program.cs
@@ -1,5 +1,7 @@
 using System.Data.SqlClient;
 using Dapper;
+using Measurement.Model;
+using MeasurementX=Measurement.Model.Measurement;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -9,15 +11,37 @@ app.MapGet("/measurement", async () =>
 {
     var conn = new SqlConnection(connStr);
     const string sql = "select * from measurement";
-    var measurements = await conn.QueryAsync(sql);
+    var measurements = await conn.QueryAsync<MeasurementX>(sql);
     return measurements;
 });
-app.MapPost("/measurement", async (Measurement.Model.Measurement measurement) =>
+app.MapGet("/measurementStats", async () =>
+{
+    var conn = new SqlConnection(connStr);
+    const string sql = @"
+        select 
+        Year(TimeStamp) as Year
+        , Max(Value) as MaxValue
+        , count(Value) as ValueCount
+        from Measurement
+        group by Year(TimeStamp)
+        ";
+    var measurements = await conn.QueryAsync<MeasurementStats>(sql);
+    return measurements;
+});
+app.MapPost("/measurement", async (MeasurementX measurement) =>
 {
     var conn = new SqlConnection(connStr);
     const string sql = "insert into measurement values (@Id, @Value, @TimeStamp, @Type, @UserId)";
     var rowsAffected = await conn.ExecuteAsync(sql, measurement);
     return rowsAffected;
 });
+app.MapDelete("/measurement/{id}", async (Guid id) =>
+{
+    var conn = new SqlConnection(connStr);
+    const string sql = "delete from measurement where id = @Id";
+    var rowsAffected = await conn.ExecuteAsync(sql, new {Id=id});
+    return rowsAffected;
+});
+
 app.UseStaticFiles();
 app.Run();
